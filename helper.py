7e384b4e7759ee23ebd6b11a39385bd482fb3e39
MosesAulbach <moses.aulbach@mailbox.org>
2023-10-14 11:48:24 +0200

    Add helper.py

---
+++ helper.py
@@ -0,0 +1,30 @@
+# Performancekennzahlen:
+
+# Teile pro Rechnung
+def calc_upt(art,trans):
+    upt = art / trans
+    return upt
+
+# Durchschnittl. Wert pro Rechnung (1 Rechnung == 1 Transaktion)
+def calc_atv(rev, trans):
+    atv = rev / trans
+    return atv
+
+# Durschnittl. Wert pro Artikel
+def calc_aur(rev, art):
+    aur = rev / art
+    return aur
+
+# Wert pro Besucher
+def calc_sot(rev, visit):
+    sot = rev / visit
+    return sot
+
+
+
+
+
+
+
+
+
